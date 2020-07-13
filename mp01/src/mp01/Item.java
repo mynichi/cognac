9b49004090dc4a402f4f170266ca530909dc5032
mynichi <86933612@qq.com>
2020-07-13 14:35:24 +0800

    modify the Class Item with constructor and getters&setters

--- mp01/src/mp01/Item.java
+++ mp01/src/mp01/Item.java
@@ -7,4 +7,48 @@ public class Item {
 	private Float quntity;
 	private Float sum;
 
+	public Item(String id, String name, Float price, Float quntity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.quntity = quntity;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public Float getQuntity() {
+		return quntity;
+	}
+
+	public void setQuntity(Float quntity) {
+		this.quntity = quntity;
+	}
+
+	public Float getSum() {
+		return quntity * price;
+	}
+
 }
